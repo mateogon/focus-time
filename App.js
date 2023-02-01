22fb7fac7eff73711cdd41631423d8b1bcebfc7b
mateogon <mateogon97@hotmail.com>
2023-02-01 16:49:55 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, SafeAreaView , Platform, StatusBar} from 'react-native';
+import Constants from 'expo-constants';
+import { colors } from './src/utils/colors';
+import { Focus } from './src/features/Focus';
+
+export default function App() {
+  return (
+    <SafeAreaView  style = {styles.container}>
+      <Focus/>
+    </SafeAreaView >
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.tomatoRed
+    
+  }
+});
